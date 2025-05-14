b731d30e2354521367c2063612c967beaf38b21a
IgorMSiqueira0 <igor.siqueira700@gmail.com>
2025-05-14 14:03:14 -0300

    att de outros path, att da mascara de input e redirect do form

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-import Inputmask from './node_modules/inputmask/dist/inputmask.es6.js';
+import Inputmask from 'https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/5.0.9/inputmask.es6.min.js';
 
-let phone = document.querySelector('#contact-phone');
-let email = document.querySelector('#contact-email');
+function applyPhoneMask() {
+	let phone = document.querySelector('#contact-phone');
 
-Inputmask({ mask: '(99) [9]9999-9999' }).mask(phone);
-Inputmask('email').mask(email);
+	Inputmask({
+		mask: ['(99) 9999-9999', '(99) 99999-9999'],
+		keepStatic: true,
+	}).mask(phone);
+}
+
+applyPhoneMask();
+
+function formWhatsappRedirect(formID) {
+	const form = document.querySelector(formID);
+
+	const name = form.querySelector('#contact-name');
+	const phone = form.querySelector('#contact-phone');
+	const email = form.querySelector('#contact-email');
+	const message = form.querySelector('#contact-message');
+
+	form.addEventListener('submit', (e) => {
+		e.preventDefault();
+		const text = `Olá Katia, meu nome é ${name.value} e tenho a seguinte mensagem:
+${message.value}
+Minhas informações de contato são:
+${email.value ? `E-mail: ${email.value}` : ''}
+Telefone: ${phone.value}`;
+
+		let encodedText = encodeURIComponent(text);
+
+		window.open(`http://wa.me/5511970472678?text=${encodedText}`, '_blank');
+	});
+}
+
+formWhatsappRedirect('#contact-form');
